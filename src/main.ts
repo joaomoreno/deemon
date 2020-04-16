45e01b259d8211e03485ac7186b961ea3b6ae7d2
João Moreno <joao.moreno@microsoft.com>
2020-04-16 11:02:17 +0200

    fix header

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+#!/usr/bin/env node
 
 import * as path from 'path';
 import * as net from 'net';
